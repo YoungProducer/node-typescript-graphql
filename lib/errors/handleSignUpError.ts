509f5bdb1de3ba9181f02159b33b7786594af93d
YoungProducer <sashabezrukovownmail@gmail.com>
2019-12-13 18:11:28 +0200

    Updated some files.

--- lib/errors/handleSignUpError.ts
+++ lib/errors/handleSignUpError.ts
@@ -1,15 +1,51 @@
 import { UserInputError } from 'apollo-server';
 import { SignUpCredentials } from '../types/auth';
 
+interface I_InvalidCredentials {
+    email: boolean;
+    password: boolean;
+    userName: boolean;
+}
+
+interface I_InvalidCredentialsMessages {
+    email: string;
+    password: string;
+    userName: string;
+}
+
 export const handleSignUpError = (error: any, credentials: SignUpCredentials) => {
     if (error) {
-        console.log({ error })
-        const { message } = error.result.errors[0];
-        const invalidArg = Object.keys(credentials).filter(arg => message.indexOf(arg) !== -1);
-        if (invalidArg) {
-            throw new UserInputError('Form Arguments invalid', {
-                invalidArg,
-            });
+        console.log(error);
+        const invalidCredentials: I_InvalidCredentials = {
+            email: false,
+            password: false,
+            userName: false,
+        };
+        const invalidCredentialsMessages: I_InvalidCredentialsMessages = {
+            email: '',
+            password: '',
+            userName: '',
+        };
+
+        if (error.result) {
+            const { message } = error.result.errors[0];
+            if (message.indexOf('email') !== -1) {
+                invalidCredentials.email = true;
+                invalidCredentialsMessages.email = message;
+            }
+            if (message.indexOf('password') !== -1) {
+                invalidCredentials.password = true;
+                invalidCredentialsMessages.password = message;
+            }
+            if (message.indexOf('userName') !== -1) {
+                invalidCredentials.userName = true;
+                invalidCredentialsMessages.userName = message;
+            }
+            if (invalidCredentials.email || invalidCredentials.password || invalidCredentials.userName) {
+                throw new UserInputError('Form Arguments invalid', {
+                    invalidCredentials,
+                });
+            }
         }
     }
     throw error;
